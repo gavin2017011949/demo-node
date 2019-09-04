8fe708c7cd6ce8bc6316295aba981fc91bc897d0
suntong <1730353402@qq.com>
2019-09-04 19:52:09 +0800

    it's so hard

---
+++ 02-global-var/01-file-dir-name.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+console.log('dir name',__dirname);
+console.log('file name',__filename);
---
+++ 03-console/01-format.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+var wd={
+  'name':'王顶',
+  'QQ':'1234567',
+  'age':41
+};
+console.log("Name:%s\t Age:%d",wd.name,wd.age);
+console.log('Wangding Info:',wd);
+console.log('name:',wd.name);
+console.log('age is ${wd.age}');
