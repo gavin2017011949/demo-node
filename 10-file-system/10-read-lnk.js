9d6605f2918f3c2cdcf032502081fbd8dd696658
suntong <1730353402@qq.com>
2019-09-20 17:20:35 +0800

    meng

---
+++ 10-file-system/10-read-lnk.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs'),
+            lnk = process.argv[2];
+
+if(typeof(lnk) === 'undefined' || process.argv.length !== 3) {
+    console.error('命令行参数不正确!');
+      process.exit(1);
+
+}
+
+try{
+    console.log('%s -> %s', lnk, fs.readlinkSync(lnk));
+
+}
+catch(e){
+    console.error(err.message);
+      process.exit(2);
+
+}
+© 2019 GitHub, Inc.
---
+++ 10-file-system/11-my-chmod.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs'),
+            mod = process.argv[2],
+                  dst = process.argv[3];
+
+//fs.chmodSync(dst, Number(mod));
+//
+try{
+  fs.chmodSync(dst, parseInt(mod, 8));
+  }
+  catch(err){
+    console.error(err.message);
+      process.exit(2);
+      }
+  }
+}
