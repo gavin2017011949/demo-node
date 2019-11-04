2a6c6da07aa9cca41309112469835e8e21d49022
gavin2017011949 <1730353402@qq.com>
2019-11-04 15:23:42 +0800

    blog

--- task/index.js
+++ task/index.js
@@ -1,129 +1,167 @@
-// const { chapterList, userList} = require('./demo1');
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-var URL = require('url');
-var qs=require("querystring");
+const http = require('http'),
+      fs   = require('fs'),
+      url = require('url'),
+      log  = console.log,
+      qs   = require('querystring');
 
+var items = [];
+const { chapterList, userList } = require('./data');
+http.createServer(function(req, res) {
+ 
 
+  log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
+  log(req.headers);
+  log('');
 
-var Id=0;
-var nowChapter = {};
+  switch(req.method) {
+    case 'GET':
+      show(req,res);
+      break;
 
-http.createServer((req,res)=>{
-if(req.url === '/login'){
-var listPath = path.join(__dirname,'login.html');
-res.writeHead(200,{'Content-Type':'text/html'});
-fs.readFile(listPath,'utf-8',(err,data)=>{
-    if(err){
-        // console.error(message);
-    }else{
-        res.end(data);
-    }
-})
-}
-else if(req.url === '/list'){
-var listPath = path.join(__dirname,'chapterList.html');
-res.writeHead(200,{'Content-Type':'text/html'});
-fs.readFile(listPath,'utf-8',(err,data)=>{
-    if(err){
-        // console.error(message);
-    }else{
-        res.end(data);
-    }
-})
-}
-else if(req.url == '/a/'){
-res.write(JSON.stringify(chapterList));
-res.end();
-}
-else if(URL.parse(req.url).pathname == '/detail'){
+    case 'POST':
+      add(req, res);
+      break;
 
-    var listPath = path.join(__dirname,'chapter.html');
-    Id=URL.parse(req.url).query.replace(/chapterId=/,"")-1;
-    res.writeHead(200,{'Content-Type':'text/html'});
-    fs.readFile(listPath,'utf-8',(err,data)=>{
-        if(err){
-            // console.error(message);
-        }else{
-            // console.log(data);
-            res.end(data);
-        }
-    })
-}
-else if(URL.parse(req.url).pathname == '/listmanager'){
-console.log(URL.parse(req.url, true).query.username);
-if(URL.parse(req.url, true).query.username == userList[0].username && URL.parse(req.url, true).query.pwd ==userList[0].pwd ){
-    var listPath = path.join(__dirname,'list.html');
-    res.writeHead(200,{'Content-Type':'text/html'});
-    fs.readFile(listPath,'utf-8',(err,data)=>{
-        if(err){
-            // console.error(message);
-        }else{
-            res.end(data);
-        }
-    })
-}else{
-    res.end('404');
-}
-
-}
-else if(req.url === '/addChapter/'){
-var listPath = path.join(__dirname,'addChapter.html');
-res.writeHead(200,{'Content-Type':'text/html'});
-fs.readFile(listPath,'utf-8',(err,data)=>{
-    if(err){
-        // console.error(message);
-    }else{
-        res.end(data);
-    }
-})
-}
-else if(req.url == '/art/'){
-res.write(JSON.stringify(chapterList));
-res.end();
-}else if(req.url == '/nowChapter/'){
-res.writeHead(200,{'Content-Type':'text/json'});
-    nowChapter=chapterList[Id];  
-    res.end(JSON.stringify(nowChapter));
-}else if(req.url == '/add'){
-console.log("收到");
-var newChapter = {};
-
-var postData = ""; 
-// 数据块接收中
- req.addListener("data", function (postDataChunk) {
-    postData += postDataChunk;
-
-    var title=qs.parse(postData).title;
-    var content=qs.parse(postData).content;
-    console.log(qs.parse(postData));
-
-    newChapter.chapterId=chapterList.length+1;
-    newChapter.chapterName=title;
-    newChapter.chapterDes=content;
-    newChapter.chapterContent=content;
-    newChapter.publishTimer= "2019-08-19";
-    newChapter.author="admin";
-    newChapter.views=1022;
-    newChapter.imgPath='';
-    chapterList.push(newChapter);
-
-});
-
-
-
-
-
-}else if(req.url !== '/'){
-var cpurl = '.'+req.url;
-res.writeHead(200,{'Content-type':"text/css"});
-fs.readFile(cpurl, function(err, data) {
-    if (err) {
-//         console.error(err);
-    }else{
-        res.end(data);
-    }
-});
-}
+    default:
+      err(res);
+      break;
+  }
 }).listen(8083);
+
+function err(res) {
+  var msg = 'Not found';
+  res.writeHead(404, {
+    'Content-Length': msg.length,
+    'Content-Type': 'text/plain'
+  });
+  res.end(msg);
+}
+
+function show(req,res) {
+  if(req.url=='/list'){
+    var html = fs.readFileSync('./chapterList.html');  
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': Buffer.byteLength(html),
+    'Access-Control-Allow-Origin': '*'
+  });
+
+  res.end(html);
+  
+  }else if(req.url=='/login'){
+    var html = fs.readFileSync('./login.html');  
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': Buffer.byteLength(html),
+    'Access-Control-Allow-Origin': '*'
+  });
+
+  res.end(html);
+  
+  }
+  else if(req.url=='/listmanager'){
+    var html = fs.readFileSync('./list.html');  
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': Buffer.byteLength(html),
+    'Access-Control-Allow-Origin': '*'
+  });
+
+  res.end(html);
+  
+  }
+  else if(req.url=='/addChapter'){
+    var html = fs.readFileSync('./addChapter.html');  
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': Buffer.byteLength(html),
+    'Access-Control-Allow-Origin': '*'
+  });
+
+  res.end(html);
+  
+  }
+  else if(url.parse(req.url).pathname=='/detail'){
+    var html = fs.readFileSync('./chapter.html');  
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': Buffer.byteLength(html),
+    'Access-Control-Allow-Origin': '*'
+  });
+
+  res.end(html);
+  
+  }
+  else if(url.parse(req.url).pathname=='/getDetail'){
+    let id = url.parse(req.url,true).query.chapterId-1;
+
+    let obj = chapterList[id];
+    res.writeHead(200,{'Content-Type': 'text/json'});
+    res.end(JSON.stringify(obj));
+  
+  }
+  else if(req.url == '/data/'){
+    let data = JSON.stringify(chapterList);
+    res.end(data);
+  }
+
+  else if(req.url !== '/'){
+    var cpurl = '.'+req.url;
+    fs.readFile(cpurl, function(err, data) {
+        if (err) {}else{
+            res.end(data);
+        }
+    });
+    }
+  
+}
+function add(req,res){
+    if(req.url == '/login'){
+      let user = '';
+      let sign = 0;
+    
+      req.on('data', (data)=>{
+        user += data;
+      });
+    
+      req.on('end', ()=>{
+        user = JSON.parse(user);
+    
+        userList.map((item)=>{
+          if(item.username == user.name && item.pwd == user.pswd){
+            sign = 1;
+            res.statusCode = 200;
+            res.end('OK');
+          }
+        });
+        if(sign == 0){
+          res.statusCode = 404;
+          res.end('ERROR')
+        }
+    
+      });
+    }
+    else if(req.url == '/add'){
+      let essay = '';
+      req.on('data',(data)=>{
+        essay += data;
+      });
+      req.on('end',()=>{
+        essay = qs.parse(essay.toString(''));
+        let item = {
+          chapterId: chapterList.length+1,
+          chapterName: essay.title || '',
+          imgPath: essay.imgPath ||'images/xixi.jpg',
+          chapterDes: essay.content || undefined,
+          chapterContent: essay.content || '',
+          publishTimer: new Date().getTime(),
+          author: essay.author || undefined,
+          views: essay.views || 2019,
+        }
+        chapterList.push(item);
+      })
+      res.write(JSON.stringify(chapterList));
+      res.end('OK');
+        }
+}
+
